a7cd2446f9ad2a285d8ff67b91903a59e75832d3
Alex Tulic <alex.tulic@gmail.com>
2019-06-01 23:04:32 -0700

    add a basic test

---
+++ client/src/components/BlogPost.test.tsx
@@ -0,0 +1,32 @@
+import {BlogPost} from "./BlogPost";
+import * as React from "react";
+import {MockedProvider, MockedResponse} from "react-apollo/test-utils";
+import {mount} from "enzyme";
+import {POST_QUERY} from "../queries/fetchPostQuery";
+import {FetchPosts} from "../queries/types/FetchPosts";
+import {Loading} from "./Loading";
+
+const mockPost: FetchPosts = {posts: [{
+    title: "Title", content: "Content", __typename: "Post",
+}]};
+
+const mocks: ReadonlyArray<MockedResponse> = [
+    {
+        request: {
+            query: POST_QUERY,
+        },
+        result: {data : mockPost},
+    },
+];
+
+describe("The BlogPost Component", () => {
+    it("renders the loading state", async () => {
+        const wrapper = mount(
+            <MockedProvider mocks={mocks} addTypename={true}>
+                <BlogPost/>
+            </MockedProvider>
+        ).childAt(0);
+
+        expect(wrapper.find(Loading).exists()).toBe(true)
+    })
+});
