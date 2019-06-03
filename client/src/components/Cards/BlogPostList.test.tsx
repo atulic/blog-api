39c9bbdcf750cf0eb47edc248e47a6b7afcc2b8a
Alex Tulic <alex.tulic@gmail.com>
2019-06-03 15:46:46 -0700

    add test for final state of blog post list

--- client/src/components/Cards/BlogPostList.test.tsx
+++ client/src/components/Cards/BlogPostList.test.tsx
@@ -1,16 +1,20 @@
 import * as React from "react";
 import { MockedProvider, MockedResponse } from "react-apollo/test-utils";
-import { mount } from "enzyme";
+import { mount, ReactWrapper } from "enzyme";
 import { POST_QUERY } from "../../queries/fetchPostQuery";
 import { FetchPosts } from "../../queries/types/FetchPosts";
 import { Loading } from "../Loading";
 import { BlogPostList } from "./BlogPostList";
+import wait from "waait";
+import { BlogCard } from "./BlogCard";
 
-const mockPost: FetchPosts = {
+let wrapper: ReactWrapper;
+
+const mockPosts: FetchPosts = {
   posts: [
     {
-      title: "Title",
       content: "Content",
+      title: "Title",
       __typename: "Post"
     }
   ]
@@ -21,18 +25,27 @@ const mocks: ReadonlyArray<MockedResponse> = [
     request: {
       query: POST_QUERY
     },
-    result: { data: mockPost }
+    result: { data: mockPosts }
   }
 ];
 
 describe("The BlogPostList Component", () => {
-  it("renders the loading state", async () => {
-    const wrapper = mount(
+  beforeEach(() => {
+    wrapper = mount(
       <MockedProvider mocks={mocks} addTypename={true}>
         <BlogPostList />
       </MockedProvider>
-    ).childAt(0);
+    );
+  });
 
+  it("renders the loading state", async () => {
     expect(wrapper.find(Loading).exists()).toBe(true);
+  });
+
+  it("renders the Blog Card and passes the result of the query", async () => {
+    await wait(0); // need to wait until next tick of event loop before MockedProvider resolves our data
+    wrapper.update();
+
+    expect(wrapper.find(BlogCard).props().post).toEqual(mockPosts!.posts![0]);
   });
 });
