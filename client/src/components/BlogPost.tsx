57c64822eaf68c6a59e19edbb507d53ba4c98c5d
Alex Tulic <alex.tulic@gmail.com>
2019-06-03 11:39:47 -0700

    material cards for posts

--- client/src/components/BlogPost.tsx
+++ client/src/components/BlogPost.tsx
@@ -4,23 +4,49 @@ import { POST_QUERY } from "../queries/fetchPostQuery";
 import { FetchPosts } from "../queries/types/FetchPosts";
 import { Loading } from "./Loading";
 import { Error } from "./Error";
-import { Box } from "@material-ui/core";
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Grid,
+  Typography
+} from "@material-ui/core";
 
-export const BlogPost: React.FC = () => (
-  <Query<FetchPosts> query={POST_QUERY}>
-    {({ loading, error, data }) => {
-      if (loading) return <Loading />;
-      if (error) return <Error />;
-      return (
-        data &&
-        data.posts &&
-        data.posts.map((post, i) => (
-          <Box key={i}>
-            <div>{post && post.title}</div>
-            <div>{post && post.content}</div>
-          </Box>
-        ))
-      );
-    }}
-  </Query>
-);
+export const BlogPost: React.FC = () => {
+  return (
+    <Query<FetchPosts> query={POST_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <Loading />;
+        if (error) return <Error />;
+
+        const postContent =
+          data &&
+          data.posts &&
+          data.posts.map((post, i) => (
+            <Grid item xs={12} sm={6} md={4}>
+              <Card key={i}>
+                <CardContent>
+                  <Typography variant="h5">{post && post.title}</Typography>
+                  <Typography variant="body1">
+                    {post && post.content}
+                  </Typography>
+                </CardContent>
+                <CardActions>
+                  <Button size="small" color="primary">
+                    Learn More
+                  </Button>
+                </CardActions>
+              </Card>
+            </Grid>
+          ));
+
+        return (
+          <Grid container spacing={2}>
+            {postContent}
+          </Grid>
+        );
+      }}
+    </Query>
+  );
+};
